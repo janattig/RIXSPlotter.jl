ea900f73e686e60c64a71015c0cf7100939a72f5
Luca Peterlini <lpeterli@l24.thp.uni-koeln.de>
2021-10-13 11:03:45 +0200

    included files

--- src/RIXSPlotter.jl
+++ src/RIXSPlotter.jl
@@ -5,6 +5,10 @@ module RIXSPlotter
     using RIXSCalculator
 
     # include files
+    include("eigensystem_plotting.jl")
+    include("spectrum_plotting.jl")
+    include("lab_system_plotting/lab_system_plotting.jl")
+    include("lab_system_plotting/momentum_transfer_plotting.jl")
     
 
 end
