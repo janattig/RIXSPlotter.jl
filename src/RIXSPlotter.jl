b1a9ef1c7dbea390e5ecf934b85b5611e14dae67
Jan Attig <jattig@thp.uni-koeln.de>
2021-10-13 16:34:43 +0200

    corrected includes

--- src/RIXSPlotter.jl
+++ src/RIXSPlotter.jl
@@ -8,8 +8,8 @@ module RIXSPlotter
     # include files
     include("eigensystem_plotting.jl")
     include("spectrum_plotting.jl")
-    include("lab_system_plotting/lab_system_plotting.jl")
-    include("lab_system_plotting/momentum_transfer_plotting.jl")
+    include("lab_system_plotting.jl")
+    include("momentum_transfer_plotting.jl")
 
 
 end
